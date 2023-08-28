20cf66f345d155e37548d191390a695d422aa5d5
Mohd-Murtaza <mohd.murtaza0101@gmail.com>
2023-08-28 14:24:22 +0530

    files

---
+++ intro.js
@@ -0,0 +1,43 @@
+console.log("Masai School");
+console.log("A Transformation in Education")
+
+
+
+
+var name="Mohd Murtaza";
+console.log(name);
+name="Mohd Nafees";
+console.log(name);
+name="Shaheen";
+console.log(name);
+
+
+var name="Mohd Murtaza";
+var age=25;
+console.log(name,age);
+console.log(typeof(name), typeof(age));
+
+
+
+var name="Mohd Murtaza";
+var school="GBSS School";
+var grade="A+";
+var section="B";
+var roll_no=1;
+var eng=91;
+var science=95;
+var math=93;
+console.log("****",`🆁 🅴🅿 🅾 🆁 🆃   🅲🅰 🆁 🅳`,"*****");
+console.log("------------------------------");
+console.log("| My Name     -", name,"|");
+console.log("| School Name -", school," |");
+console.log("| Grade       -", grade,"          |");
+console.log("| Section     -", section,"           |");
+console.log("| Roll No.    -", roll_no,"           |");
+console.log("------------------------------");
+console.log("$$$$$$$$$",`🅼 🅰 🆁 🅺 🆂`,"$$$$$$$$$$");
+console.log("------------------------------");
+console.log("| Englinsh    -", eng,"          |");
+console.log("| Science     -", science,"          |");
+console.log("| Math        -", math,"          |");
+console.log("------------------------------");
